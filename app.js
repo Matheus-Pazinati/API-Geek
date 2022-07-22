019f3870ab2d29a46cebfa079a2cb99cf5a17e26
Matheus Pazinati <matheuspazinati@gmail.com>
2022-07-22 15:43:32 -0300

    Habilita cors

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
 app.use(morgan('dev'))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 const port = process.env.PORT || 3000;
 
